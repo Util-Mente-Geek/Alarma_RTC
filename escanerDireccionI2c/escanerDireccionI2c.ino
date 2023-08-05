d72bc20c09a5d533af94b396a1ffb94ee7646ff5
Martin Segoviano <segoviano_martin@hotmail.com>
2023-08-05 12:52:30 -0600

    Codigo para saber la direccion i2c de tus dispositivos

---
+++ escanerDireccionI2c/escanerDireccionI2c.ino
@@ -0,0 +1,57 @@
+/*********
+ * AxeDoor
+ * Codigo para Escanear los puertos i2c de tu micro y te regresa la direccion de los mismos.
+ * Microcontrolador: Esp32 / Arduino
+ * PaginaWeb: www.utilmentegeek.com
+ * YouTube: https://www.youtube.com/channel/UCBqfcl_tue6Zsk4Hc3BErWg
+ * Instagram: https://www.instagram.com/utilmentegeek/
+ * 
+ * Recuerda que tambien pudes contribuir a la mejora de este codigo en GitHub
+ * Y apoyar el desarrollo de estos proyectos así como resolver dudas sobre el codigo
+ * en nuestras redes socilaes @Util Mente Geek.
+ * 
+ * Creditos correspondientes por el codigo:
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <Wire.h>
+ 
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+  delay(5000);          
+}
